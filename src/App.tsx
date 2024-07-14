c389f781369528f8d9515e3b34d403a12101a7ce
Angelina Wang <angeliw9@uci.edu>
2024-07-13 17:38:49 -0700

    Built the basic input fields for the business name and year-end date.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Expenses Summary";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content"></div>
+        <h1>{ title }</h1>
+        <h2>Enter your business name:</h2>
+        <input type="text"/>
+        <h2>Enter your year-end date:</h2>
+        <input type="text"/>
+        <br/>
+        <button>Submit</button>
     </div>
   );
 }
